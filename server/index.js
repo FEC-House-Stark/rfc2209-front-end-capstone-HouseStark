294c26eded0b618e0cc6de3226be03ce8149b19f
Ricardo Vargas <richi_v@aol.com>
2022-10-12 16:12:18 -0500

    set up server index and client index

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
-console.log('stark')
+const express = require("express");
+const path = require("path");
+
+let port = 1128;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../client/dist")));
+app.use(express.json());
+
+
+app.listen(port, function () {
+  console.log(`listening on port ${port}`);
+});
