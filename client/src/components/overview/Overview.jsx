1a4bcc688761ec98ce7f2707d97ab8e992466f54
Georgina Lin <xialin1@g.ucla.edu>
2022-10-12 16:56:51 -0500

    set up client side directory

---
+++ client/src/components/overview/Overview.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Overview = (props) =>  {
+
+  return (
+    <div>Overview</div>
+  )
+}
+
+export default Overview;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+// import Overview from './components/overview/Overview.jsx';
+// import QandA from './components/Q&A/QandA.jsx';
+// import Reviews from './components/R&R/Reviews.jsx';
+// import Related from './components/Related/Related.jsx'
 
 
 const App = () => {
@@ -10,6 +13,10 @@ const App = () => {
   return (
     <div>
       <h1>House Stark</h1>
+      {/* <Overview/>
+      <QandA/>
+      <Reviews/>
+      <Related/> */}
     </div>
   )
 
