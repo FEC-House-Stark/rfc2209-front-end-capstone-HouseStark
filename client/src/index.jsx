6c1ea03f4008acbec209b246d044fb9e708514b0
Jojo Ortiz <ortiz.jroman@gmail.com>
2022-10-14 15:39:58 -0500

    implement states and GET requests for reviews, products, and styles

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -10,8 +10,14 @@ import Related from './components/Related/Related.jsx';
 const host_url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfc/'
 
 const App = () => {
-  const [product_id, setProductId] = useState(66643);
-  console.log('Initializing app...:', process.env.TOKEN);
+  const [product_id, setProductId] = useState(66644);
+  const [numReviews, setNumReviews] = useState(0);
+  const [avgRating, setAvgRating] = useState(0);
+  const [characteristics, setCharacteristics] = useState({});
+  const [recommended, setRecommended] = useState({});
+  const [productInfo, setProductInfo] = useState({});
+  const [styles, setStyles] = useState({});
+
   useEffect(() => {
     let config = {
       headers: {
@@ -20,15 +26,54 @@ const App = () => {
       params: { product_id },
     }
 
+    //calculate numReviews
+    const calcReviewData = (ratings) => {
+            //calculate numReviews
+      let totalReviews = 0;
+      let totalSum = 0
+      //iterate over ratings object
+      for (const rating in ratings) {
+        totalSum += rating * Number(ratings[rating]);
+        totalReviews += Number(ratings[rating]);
+      }
+      console.log('totalReviews: ', totalReviews, ' totalSum:', totalSum, ' avgRating:', totalSum/totalReviews);
+      //set numReviews
+      setNumReviews(totalReviews);
+      //set avgRating
+      let avg = Number((totalSum/totalReviews).toFixed(2));
+      setAvgRating(avg);
+    }
 
     axios.get(host_url + 'reviews/meta', config)
     .then((result) => {
       console.log('GET reviews/meta result: ', result.data);
+      calcReviewData(result.data.ratings);
+      //set characteristics
+      setCharacteristics(result.data.characteristics);
+      //set recommended
+      setRecommended(result.data.recommended);
     })
+    //set st
     .catch((err) => {
       console.log('Initialize GET ERROR:', err);
     })
-  }, []);
+    axios.get(host_url + 'products/' + product_id, config)
+    .then((result)=> {
+      console.log('GET /products/:product_id results', result.data);
+      setProductInfo(result.data);
+    })
+    .catch((err) => {
+      console.log('GET /products/:product_id ERROR:', err);
+    });
+    axios.get(host_url + 'products/' + product_id + '/styles', config)
+    .then((result)=> {
+      console.log('GET /products/:product_id/styles results', result.data);
+      setStyles(result.data);
+    })
+    .catch((err) => {
+      console.log('GET /products/:product_id/styles ERROR:', err);
+    });
+  }, [product_id]);
 
 
   return (
