00beb0c3fa38a204e01aeb6e0ddccb4667c33212
Ricardo Vargas <richi_v@aol.com>
2022-10-17 18:00:10 -0500

    Make an axios request for related IDs and for the products related to those IDs

---
+++ client/src/components/Related/ProductList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ProductCards = styled.div`
+display: flex;
+`;
+
+const ProductList = () => {
+  return (
+    <ProductCards>
+      ProductList
+    </ProductCards>
+  )
+}
+export default ProductList;
--- client/src/components/Related/Related.jsx
+++ client/src/components/Related/Related.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import ProductList from './ProductList.jsx';
+import OutfitList from './OutfitList.jsx';
+import axios from 'axios'
 
-const Related = (props) =>  {
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+`;
+
+const host_url = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfc/'
+const config = {
+  headers: {
+    'Authorization': process.env.TOKEN,
+  }
+}
+const Related = ({ handleClick, product_id, numReviews, avgRating, productInfo, styles }) => {
+  const [related_ids, setRelatedIds] = useState([]);
+  const [related, setRelated] = useState([]);
+
+  useEffect(() => {
+    axios(host_url + `products/${product_id}/related`, config)
+      .then((res) => setRelatedIds(res.data))
+      .catch((err) => console.log(err))
+  }, [])
+
+  useEffect(() => {
+    if (related_ids.length) {
+      let promises = related_ids.map((id) => {
+        return axios(host_url + `products/${id}`, config)
+      })
+      Promise.all(promises)
+        .then((res) => setRelated(res))
+    }
+  }, [related_ids])
 
   return (
-    <div>Related</div>
+    <Container>
+      <ProductList />
+      <OutfitList />
+    </Container>
   )
 }
 
