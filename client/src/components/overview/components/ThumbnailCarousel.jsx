1dc65df34c3d348f9ed17676d3fc23d2b3c5c8e6
Jojo Ortiz <ortiz.jroman@gmail.com>
2022-10-20 17:39:56 -0500

    change arrow icons for left/right vs. thumbnail in ArrowButton.jsx

--- client/src/components/overview/components/ThumbnailCarousel.jsx
+++ client/src/components/overview/components/ThumbnailCarousel.jsx
@@ -7,8 +7,8 @@ const { useState, useEffect } = React;
 
 const ThumbnailCarousel = ({ photos, photoIndex, handleClick, height }) => {
   const gallery_height = height;
-  const carousel_height = 350;
-  const n_max_thumbnails = 7;
+  const carousel_height = 400;
+  const n_max_thumbnails = 4;
   const min_thumbnail_index = n_max_thumbnails - 1;
   let carousel_spacing = carousel_height / n_max_thumbnails;
   const thumbnail_width = .8 * carousel_spacing;
