5d841f236411ea9cd2554b6b632c3fcf24a32be7
JustinTurkaly <jturkaly18@gmail.com>
2022-10-24 19:34:59 -0500

    Made styling change to bar graph

--- client/src/components/R&R/Breakdown.jsx
+++ client/src/components/R&R/Breakdown.jsx
@@ -1,9 +1,10 @@
 import React, { useRef, useState, useEffect } from "react";
 
 
-const Breakdown = ({avgRating, fullReviewList, numReviews, characteristics}) => {
+const Breakdown = ({ avgRating, fullReviewList, numReviews, characteristics }) => {
   const [starCountObj, setStarCountObj] = useState({});
   const [calcCharact, setCalcCharact] = useState([]);
+  const [sum, setSum] = useState(0);
 
   var recommendPercentageCalculator = () => {
     let count = 0;
@@ -29,10 +30,15 @@ const Breakdown = ({avgRating, fullReviewList, numReviews, characteristics}) => 
       5: 0
     }
 
+    let total = 0;
+    let percentange;
+
     for (let i = 0; i < fullReviewList.length; i++) {
       countObj[fullReviewList[i].rating] += 1
+      total += 1
     }
     // console.log('TEST', countObj)
+    setSum(total)
     return countObj
   }
 
@@ -46,43 +52,70 @@ const Breakdown = ({avgRating, fullReviewList, numReviews, characteristics}) => 
     // console.log(result)
   }
 
-  var barRender = (starCount) => {
+  var percentageCalc = (star) => {
+
+    let percentange = starCountObj[star] / sum
+    return percentange
+  }
+
+  var barRender = (star) => {
+
+    // console.log(percentageCalc(4))
+    // console.log(starCount * 50)
+    let backgroundBarStyle = {
+      height: '15px',
+      width: `${415}px`,
+      backgroundColor: '#adb5bd',
+      border: '1px solid lightgrey',
+      borderRadius: '0px',
+      boxShadow: '0 0 15px 1px rgba(0, 0, 0, 0.4)',
+      position: 'relative'
+    }
+
     let barStyle = {
       height: '15px',
-      width: `${starCount * 50}px`,
+      width: `${415 * percentageCalc(star)}px`,
       backgroundColor: '#4D6A6D',
       border: '1px solid lightgrey',
       borderRadius: '6px',
-      boxShadow: '0 0 15px 1px rgba(0, 0, 0, 0.4)'
+      position: 'absolute',
     }
+
+
     return (
-      <div style={barStyle}></div>
+      <div style={backgroundBarStyle}><div style={barStyle}></div></div>
     )
   }
 
+  let containerStyle = {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr 1fr',
+    overflow: 'hidden',
+    whiteSpace: 'nowrap',
+  }
   return (
     <>
-    <h2>Ratings & Reviews</h2>
-    <h1 className='numberRating'>
-      {avgRating}
-    </h1>
-    <div>
-      {`${recommendPercentageCalculator()}% of reviews recommend this product`}
-    </div>
-    <div>
-      <li>{`5 Stars`}{barRender(starCountObj[5])}</li>
-      <li>{`4 Stars`}{barRender(starCountObj[4])}</li>
-      <li>{`3 Stars`}{barRender(starCountObj[3])}</li>
-      <li>{`2 Stars`}{barRender(starCountObj[2])}</li>
-      <li>{`1 Stars`}{barRender(starCountObj[1])}</li>
-      {calcCharact.map((item, i) => {
-        return (
-          <li key={i}>
-            {item}
-          </li>
-        )
-      })}
-    </div>
+      <h2>Ratings & Reviews</h2>
+      <h1 className='numberRating'>
+        {avgRating}
+      </h1>
+      <div>
+        {`${recommendPercentageCalculator()}% of reviews recommend this product`}
+      </div>
+      <div>
+        <li style={containerStyle}>{`5 Stars`}{barRender(5)}</li>
+        <li style={containerStyle}>{`4 Stars`}{barRender(4)}</li>
+        <li style={containerStyle}>{`3 Stars`}{barRender(3)}</li>
+        <li style={containerStyle}>{`2 Stars`}{barRender(2)}</li>
+        <li style={containerStyle}>{`1 Stars`}{barRender(1)}</li>
+        {calcCharact.map((item, i) => {
+          return (
+            <li key={i}>
+              {item}
+            </li>
+          )
+        })}
+      </div>
     </>
   );
 };
